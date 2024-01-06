b62bab774dfd434747c1665db59f50e83707efdb
north238 <fumiyama02@yahoo.co.jp>
2024-01-06 18:26:40 +0900

    ページネーションの作成

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CommentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostsController::class, 'index'])->name('top');
+Route::resource('posts', PostsController::class)->only(['create', 'store']);
+Route::resource('posts', PostsController::class)->only(['create', 'store', 'show']);
+Route::resource('comments', CommentsController::class)->only(['store']);
+Route::resource('posts', PostsController::class)->only(['create', 'store', 'show', 'edit', 'update']);
+Route::resource('posts', PostsController::class)->only(['create', 'store', 'show', 'edit', 'update', 'destroy']);
